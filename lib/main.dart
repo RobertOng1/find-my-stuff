6db491901c26977758c10ed613434e9ab180a11f
Rayza Indafri Yahya <indafrireza@gmail.com>
2025-10-19 15:48:37 +0700

    Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,61 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
-      home: HomePage()
+      home: MainNavbar()
+    );
+  }
+  
+}
+
+class MainNavbar extends StatefulWidget {
+  @override
+  _MainNavbarState createState() => _MainNavbarState();
+}
+
+class _MainNavbarState extends State<MainNavbar> {
+  int _selectedIndex = 0;
+
+
+  static final List<Widget> _pages = <Widget>[
+    HomePage(), 
+    Center(child: Text('Report Lost Item')),
+    Center(child: Text('Find Lost Item')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.report),
+            label: 'Report',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Find',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
